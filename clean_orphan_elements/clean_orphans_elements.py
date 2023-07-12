24cfa8c6c66a98964523f962ca309b2f0ca4426a
LE SAULNIER Kevin <kevin.lesaulnier@rte-france.com>
2023-07-12 09:50:57 +0200

    feat: clean orphan elements : networks, fiters and contingency-lists for now
    Signed-off-by: LE SAULNIER Kevin <kevin.lesaulnier@rte-france.com>

---
+++ clean_orphan_elements/clean_orphans_elements.py
@@ -0,0 +1,29 @@
+import argparse
+
+from functions.clean_orphan_contingency_lists import delete_orphan_contingency_lists
+from functions.clean_orphan_filters import delete_orphan_filters
+from functions.clean_orphan_networks import delete_orphan_network
+
+from script_mode import ScriptMode
+
+parser=argparse.ArgumentParser(description='Send requests to the gridsuite services to remove orphan elements',)
+
+parser.add_argument(("--mode"), help="test mode (default) will not execute any modification request", choices=[ScriptMode.TEST.value, ScriptMode.EXEC.value], nargs='?', type=str, const=ScriptMode.TEST, default=ScriptMode.TEST)
+
+args=parser.parse_args()
+script_mode = args.mode
+
+if(script_mode == ScriptMode.TEST):
+    print("Orphans deletion script will run without deleting anything (test mode)")
+else:
+    print("Orphans deletion script (exec mode)")
+
+print("\n\n")
+
+delete_orphan_network(script_mode)
+
+delete_orphan_contingency_lists(script_mode)
+
+delete_orphan_filters(script_mode)
+
+
---
+++ clean_orphan_elements/script_mode.py
@@ -0,0 +1,5 @@
+from enum import Enum
+
+class ScriptMode(Enum):
+    TEST = "test"
+    EXEC = "exec"
