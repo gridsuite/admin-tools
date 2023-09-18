3d50ac0b6adee1b7fc671f9de136f832ea29bf4e
Hugo Marcellin <hugo.marcelin@rte-france.com>
2023-09-18 19:01:20 +0200

    Change label to clarify behaviour

--- functions/clean_computation_results.py
+++ functions/clean_computation_results.py
@@ -5,10 +5,10 @@ def delete_computation_results(dry_run, computationType):
     print("/// " + computationType + " results deletion ///")
     if dry_run: 
         resultsCount = requests.delete(constant.DELETE_COMPUTATION_RESULTS, params={'type': computationType, "dryRun": "true"})
-        print("Here's the count of stored results : " + str(resultsCount.json()))
+        print("Here's the count of stored computation results : " + str(resultsCount.json()))
     else :
         result = requests.delete(constant.DELETE_COMPUTATION_RESULTS, params={'type': computationType, "dryRun": "false"})
         if result.ok :
-            print("Here's the count of deleted results : " + str(result.json()))
+            print("Here's the count of processed nodes results : " + str(result.json()))
         else :
             print("An error occured : " + str(result.json()))
