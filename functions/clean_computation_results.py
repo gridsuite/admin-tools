eae4920f21b87dfcb953328879e9d1613174ef8f
Hugo Marcellin <hugo.marcelin@rte-france.com>
2023-09-19 17:02:25 +0200

    Add licence

--- functions/clean_computation_results.py
+++ functions/clean_computation_results.py
@@ -1,5 +1,16 @@
+#
+# Copyright (c) 2023, RTE (http://www.rte-france.com)
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
 import requests
 import constant
+
+#
+# @author Hugo Marcellin <hugo.marcelin at rte-france.com>
+#
 
 def delete_computation_results(dry_run, computationType):
     print("/// " + computationType + " results deletion ///")
