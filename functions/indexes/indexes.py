292c076cd4f8ab7d0e66499f06beb97ef7f97e83
Hugo Marcellin <hugo.marcelin@rte-france.com>
2023-09-20 11:42:31 +0200

    Indexing to indexes

---
+++ functions/indexes/indexes.py
@@ -0,0 +1,25 @@
+#
+# Copyright (c) 2023, RTE (http://www.rte-france.com)
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+import requests
+import constant
+
+#
+# @author Hugo Marcellin <hugo.marcelin at rte-france.com>
+#
+
+def delete_equipments_indexes(dry_run, computationType):
+    print("/// Delete studies equipments indexes ///")
+    if dry_run: 
+        resultsCount = requests.delete(constant.DELETE_COMPUTATION_RESULTS, params={'type': computationType, "dryRun": "true"})
+        print("Here's the count of stored computation results : " + str(resultsCount.json()))
+    else :
+        result = requests.delete(constant.DELETE_COMPUTATION_RESULTS, params={'type': computationType, "dryRun": "false"})
+        if result.ok :
+            print("Here's the count of processed nodes results : " + str(result.json()))
+        else :
+            print("An error occured : " + str(result.json()))
