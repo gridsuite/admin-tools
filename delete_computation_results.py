81acb71e563641f86a66e268fd58e038a1b9ef70
Hugo Marcellin <hugo.marcelin@rte-france.com>
2023-10-02 16:07:50 +0200

    Remove merge marks

--- delete_computation_results.py
+++ delete_computation_results.py
@@ -7,11 +7,7 @@
 
 import argparse
 import constant
-<<<<<<< HEAD
-from functions.delete_computation_results import delete_computation_results 
-=======
 from functions.delete_computation_results.delete_computation_results import delete_computation_results 
->>>>>>> origin/main
 
 #
 # @author Hugo Marcellin <hugo.marcelin at rte-france.com>
