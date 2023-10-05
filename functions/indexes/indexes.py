2a1775852b4031c5691144be83bf4072ce29f8ab
sBouzols <sylvain.bouzols@gmail.com>
2023-10-05 12:17:06 +0200

    Adapt logs
    Signed-off-by: sBouzols <sylvain.bouzols@gmail.com>

--- functions/indexes/indexes.py
+++ functions/indexes/indexes.py
@@ -13,13 +13,13 @@ import constant
 #
 
 def delete_indexed_equipments(dry_run):
-    print("/// Studies indexed equipments deletion ///")
+    print("/// Studies indexed equipments and tomstoned deletion ///")
     if dry_run: 
         resultsCount = requests.delete(constant.DELETE_STUDIES_INDEXED_EQUIPMENTS, params={"dryRun": "true"})
-        print("Here's the count of stored indexed equipments : " + str(resultsCount.json()))
+        print("Here's the count of stored indexed equipments and tomstoned : " + str(resultsCount.json()))
     else :
         result = requests.delete(constant.DELETE_STUDIES_INDEXED_EQUIPMENTS, params={"dryRun": "false"})
         if result.ok :
-            print("Here's the count of deleted indexed equipments : " + str(result.json()))
+            print("Here's the count of deleted indexed equipments and tomstoned : " + str(result.json()))
         else :
             print("An error occured : " + str(result.json()))
