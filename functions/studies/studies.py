bb4f11374d8cf49d258928aa48ce7c7b3cb07635
achour94 <berrahmaachour@gmail.com>
2024-05-23 14:31:16 +0200

    delete orphaned indexed equipments (#14)

--- functions/studies/studies.py
+++ functions/studies/studies.py
@@ -12,8 +12,28 @@ import constant
 # @author Sylvain Bouzols <sylvain.bouzols at rte-france.com>
 #
 
+
 def get_all_studies_uuid():
     return requests.get(constant.GET_STUDIES).json()
+
+def get_all_orphan_indexed_equipments_network_uuids():
+    return requests.get(constant.GET_ALL_ORPHAN_INDEXED_EQUIPMENTS_NETWORK_UUIDS).json()
+
+def delete_indexed_equipments(networkUuid):
+    try:
+        result = requests.delete(url = constant.DELETE_STUDY_INDEXED_EQUIPMENTS_BY_NETWORK_UUID.format(networkUuid = networkUuid))
+
+        # Check if the response status code indicates success
+        if result.status_code == 200:
+            print(f"Successfully deleted indexed equipments for network UUID: {networkUuid}")
+            return True
+        else:
+            print(f"Failed to delete indexed equipments for network UUID: {networkUuid}. "
+                  f"Response status code: {result.status_code} - Response content: {result.content}")
+            return False
+    except requests.exceptions.RequestException as e:
+        print(f"Exception occurred while deleting indexed equipments for network UUID: {networkUuid}. Exception: {e}")
+        return False
 
 def invalidate_nodes_builds(studyUuid):
     return requests.delete(constant.DELETE_STUDY_NODES_BUILDS.format(studyUuid = studyUuid))
