68bfa5350b803beecaf86d5ad86a07c9664183c1
sBouzols <sylvain.bouzols@gmail.com>
2024-05-28 15:15:53 +0200

    fix get_eleasticsearch_host usage for delete_indexed_equipments.py
    Signed-off-by: sBouzols <sylvain.bouzols@gmail.com>

--- delete_indexed_equipments.py
+++ delete_indexed_equipments.py
@@ -52,7 +52,7 @@ print("\n")
 # Just getting an enlightening url opportunistically from here because it exists
 # TODO better ?
 plateformName = get_plateform_info()['redirect_uri']
-elasticsearch_host = get_eleasticsearch_host()
+elasticsearch_host = get_eleasticsearch_host(constant.STUDY_SERVER_HOSTNAME)
 # TODO don't parse here, instead have the server return structured information
 elasticsearch_ip = socket.gethostbyname(elasticsearch_host.split(':')[0])
 # TODO we force http but should get this protocol from the server, some servers are not exposed on http but only https for example
