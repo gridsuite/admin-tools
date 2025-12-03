35af06cb73ae0b38fb489a726b749491dce415f3
Etienne LESOT <etienne.lesot@rte-france.com>
2025-12-03 17:05:15 +0100

    add authentification for elastocsearch
    Signed-off-by: Etienne LESOT <etienne.lesot@rte-france.com>

--- scripts/functions/indexes/elasticsearch.py
+++ scripts/functions/indexes/elasticsearch.py
@@ -4,11 +4,12 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #
-
+import os
 import requests
 import constant
 import socket
 
+from requests.auth import HTTPBasicAuth
 from ..utils import prettyprint
 
 
@@ -23,7 +24,7 @@ def get_elasticsearch_host(serverHostName):
         if constant.DEV :
             return constant.DEV_ELASTICSEARCH_IP, constant.DEV_ELASTICSEARCH_URL 
         else:
-            elasticsearch_host = requests.get(constant.GET_ELASTICSEARCH_HOST.format(serverHostName = serverHostName)).text
+            elasticsearch_host = requests.get(constant.GET_ELASTICSEARCH_HOST.format(serverHostName = serverHostName), auth=__get_authentification()).text
             # TODO don't parse here, instead have the server return structured information
             elasticsearch_ip = socket.gethostbyname(elasticsearch_host.split(':')[0])
             # TODO we force http but should get this protocol from the server, some servers are not exposed on http but only https for example
@@ -33,9 +34,16 @@ def get_elasticsearch_host(serverHostName):
         print(e)
         return ""
 
+
+def __get_authentification():
+    login = os.environ['ELASTICSEARCH_LOGIN']
+    password = os.environ['ELASTICSEARCH_PASSWORD']
+    return HTTPBasicAuth(login, password)
+
+
 def check_status_elasticsearch(url):
     try:
-        result = requests.get(url + '/')
+        result = requests.get(url + '/', auth=__get_authentification())
         if not result.ok :
             print("An error occured : ")
             prettyprint(result)
@@ -54,7 +62,7 @@ def check_status_elasticsearch(url):
 def request_elasticsearch(method, url):
     try:
         print(method + " " + url)
-        result = requests.request(method, url)
+        result = requests.request(method, url, auth=__get_authentification())
         if not result.ok :
             print("An error occured : ")
             prettyprint(result)
@@ -69,4 +77,4 @@ def request_elasticsearch(method, url):
 
 def expunge_deletes(elasticsearchHost, indexName):
     print("ES Force merge : " + constant.ES_FORCE_MERGE.format(elasticsearchHost = elasticsearchHost, indexName = indexName) + "?only_expunge_deletes=true")
-    result = requests.post(url = constant.ES_FORCE_MERGE.format(elasticsearchHost = elasticsearchHost, indexName = indexName), params={'only_expunge_deletes': 'true'})
+    result = requests.post(url = constant.ES_FORCE_MERGE.format(elasticsearchHost = elasticsearchHost, indexName = indexName), params={'only_expunge_deletes': 'true'}, auth=__get_authentification())
