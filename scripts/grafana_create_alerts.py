14fb5893b9550bac55a2ed71b58abaf3bdf4a7bc
Slimane Amar <63394973+SlimaneAmar@users.noreply.github.com>
2026-04-01 10:35:12 +0200

    Consolidate supervision (#56)
    Co-authored-by: Slimane AMAR <slimane.amar_externe@rte-france.com>

--- scripts/grafana_create_alerts.py
+++ scripts/grafana_create_alerts.py
@@ -14,14 +14,17 @@ from functions.grafana.alerts import create_alert_rule
 # @author Kevin Le Saulnier <kevin.lesaulnier at rte-france.com>
 # @author Slimane Amar <slimane.amar at rte-france.com>
 #
-GRAFANA_ALERTS_DIR = 'resources/grafana/alerts'
+GRAFANA_ALERTS_DIR = 'resources/grafana/alerts/'
+GRAFANA_LOGS_ALERTS_DIR = GRAFANA_ALERTS_DIR + 'logs'
+GRAFANA_METRICS_ALERTS_DIR = GRAFANA_ALERTS_DIR + 'metrics'
 
 parser = argparse.ArgumentParser(description='Send requests to the grafana to create alert-rules into one folder - rule-groups are automatically created if not already existing')
 group = parser.add_mutually_exclusive_group(required=True)
-group.add_argument("-a", "--all", help="create all alert rules", action="store_true")
+group.add_argument("-l", "--logs", help="create all alert rules for logs (datasource loki)", action="store_true")
+group.add_argument("-m", "--metrics", help="create all alert rules for metrics (datasource prometheus)", action="store_true")
 group.add_argument("-f", "--file", help="path of the alert rule to create", action='append')
 parser.add_argument("-p", "--parent_folder_uid", help="uid of parent folder", required=True)
-parser.add_argument("-d", "--datasource_uid", help="uid of datasource", required=True)
+parser.add_argument("-d", "--datasource_uid", help="uid of datasource (loki or prometheus)", required=True)
 
 args = parser.parse_args()
 files = args.file
@@ -30,8 +33,8 @@ datasource_uid = args.datasource_uid
 
 print("Grafana alert rules creation")
 
-if args.all:
-    for path, _, files in os.walk(GRAFANA_ALERTS_DIR):
+if args.logs or args.metrics:
+    for path, _, files in os.walk(GRAFANA_LOGS_ALERTS_DIR if args.logs else GRAFANA_METRICS_ALERTS_DIR):
         for file in files:
             print("")
             create_alert_rule(os.path.join(path, file), root_folder_uid, datasource_uid, overwrite=True)
