05c1d6e76167995a9129a65dcbd7eaa79490893a
Mathieu DEHARBE <mathieu.deharbe@rte-france.com>
2026-06-23 16:19:17 +0200

    coderabbit review
    Signed-off-by: Mathieu DEHARBE <mathieu.deharbe@rte-france.com>

--- scripts/sync-composites-names.py
+++ scripts/sync-composites-names.py
@@ -7,7 +7,9 @@ parser = argparse.ArgumentParser(description='Takes the name of the composites e
 print("Getting composites lists from directory-server: " + constant.GET_DIRECTORY_ELEMENTS)
 directory_composites_response = requests.get(
     constant.GET_DIRECTORY_ELEMENTS,
-    params={"elementType": "MODIFICATION"})
+    params={"elementType": "MODIFICATION"},
+    timeout=30)
+directory_composites_response.raise_for_status()
 directory_composites_response_json = directory_composites_response.json()
 
 # updates their name (with their current name) -> it will now sync them with their name in netmod-server
@@ -15,4 +17,9 @@ directory_composites_pairs = [(element["elementUuid"], element["elementName"]) f
 print("Updates the following " + str(len(directory_composites_pairs)) + " composites names : ")
 for uuid, name in directory_composites_pairs:
     print(" - ", uuid, '-> ' + name)
-    response = requests.put(url = constant.PUT_COMPOSITES.format(uuid = uuid), json={}, params={"name": name})
+    response = requests.put(
+        url = constant.PUT_COMPOSITES.format(uuid = uuid),
+        json={},
+        params={"name": name},
+        timeout=30)
+    response.raise_for_status()
