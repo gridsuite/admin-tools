ea26825f4c4ebc9db7a5e5087a875e799d0315e5
Mathieu DEHARBE <mathieu.deharbe@rte-france.com>
2026-06-24 15:17:21 +0200

    move to migration folder
    Signed-off-by: Mathieu DEHARBE <mathieu.deharbe@rte-france.com>

--- scripts/migrations/migrate_v237_sync-composites-names.py
+++ scripts/migrations/migrate_v237_sync-composites-names.py
@@ -9,12 +9,17 @@ directory_composites_response = requests.get(
     constant.GET_DIRECTORY_ELEMENTS,
     params={"elementType": "MODIFICATION"},
     timeout=30)
-directory_composites_response.raise_for_status()
+if 200 == directory_composites_response.status_code:
+    print("Success")
+else:
+    print("Failed : {directory_composites_response.status_code} - {directory_composites_response.reason}")
+    directory_composites_response.raise_for_status()
+
 directory_composites_response_json = directory_composites_response.json()
 
 # updates their name (with their current name) -> it will now sync them with their name in netmod-server
 directory_composites_pairs = [(element["elementUuid"], element["elementName"]) for element in directory_composites_response_json]
-print("Updates the following " + str(len(directory_composites_pairs)) + " composites names : ")
+print("Updates the following " + str(len(directory_composites_pairs)) + " composites names in modification-server : ")
 for uuid, name in directory_composites_pairs:
     print(" - ", uuid, '-> ' + name)
     response = requests.put(
@@ -22,4 +27,7 @@ for uuid, name in directory_composites_pairs:
         json={},
         params={"name": name},
         timeout=30)
-    response.raise_for_status()
+    if 200 == response.status_code:
+        print("Success")
+    else:
+        print("Failed : {response.status_code} - {response.reason}")
